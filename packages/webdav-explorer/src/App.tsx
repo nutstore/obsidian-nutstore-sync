5333fdc05a126709a3fb966f7e2006a4c77fbb78
miaobuao <miaobuao@outlook.com>
2025-03-19 10:33:54 +0800

    fix: refresh list after create new folder

--- packages/webdav-explorer/src/App.tsx
+++ packages/webdav-explorer/src/App.tsx
@@ -1,7 +1,7 @@
 import { createMediaQuery } from '@solid-primitives/media'
 import path from 'path'
 import { createSignal, Show } from 'solid-js'
-import FileList, { FileStat } from './components/FileList'
+import { createFileList, FileStat } from './components/FileList'
 import NewFolder from './components/NewFolder'
 import { t } from './i18n'
 
@@ -35,67 +35,33 @@ function App(props: AppProps) {
 		)
 	}
 
-	const SingleCol = () => (
-		<div class="flex-1 flex flex-col overflow-y-auto scrollbar-hide">
-			<Show when={showNewFolder()}>
-				<NewFolder
-					class="mt-1"
-					onCancel={() => setShowNewFolder(false)}
-					onConfirm={async (name) => {
-						const target = path.join(cwd() ?? '/', name)
-						await props.fs.mkdirs(target)
-						setShowNewFolder(false)
-					}}
-				/>
-			</Show>
-			<FileList
-				fs={props.fs}
-				path={cwd() ?? ''}
-				onClick={(f) => enter(f.path)}
-			/>
-		</div>
-	)
-
-	// @ts-ignore
-	const DoubleCol = () => {
-		const fst = () => stack()?.at(-2)!
-		const snd = () => stack()?.at(-1)!
+	const SingleCol = () => {
+		const list = createFileList()
 		return (
-			<div class="flex-1 flex overflow-auto">
-				<div class="flex-1 overflow-y-auto scrollbar-hide">
-					<FileList
-						fs={props.fs}
-						path={fst()}
-						onClick={(f) => {
-							setStack([...stack().slice(0, stack().length - 1), f.path])
+			<div class="flex-1 flex flex-col overflow-y-auto scrollbar-hide">
+				<Show when={showNewFolder()}>
+					<NewFolder
+						class="mt-1"
+						onCancel={() => setShowNewFolder(false)}
+						onConfirm={async (name) => {
+							const target = path.join(cwd() ?? '/', name)
+							await props.fs.mkdirs(target)
+							setShowNewFolder(false)
+							list.refresh()
 						}}
 					/>
-				</div>
-				<div class="h-full w-1px bg-[var(--interactive-accent)] mx-1"></div>
-				<div class="flex-1 flex flex-col overflow-y-auto scrollbar-hide">
-					<Show when={showNewFolder()}>
-						<NewFolder
-							class="mt-1"
-							onCancel={() => setShowNewFolder(false)}
-							onConfirm={async (name) => {
-								const target = path.join(snd() ?? '/', name)
-								await props.fs.mkdirs(target)
-								setShowNewFolder(false)
-							}}
-						/>
-					</Show>
-					<FileList fs={props.fs} path={snd()} onClick={(f) => enter(f.path)} />
-				</div>
+				</Show>
+				<list.FileList
+					fs={props.fs}
+					path={cwd() ?? ''}
+					onClick={(f) => enter(f.path)}
+				/>
 			</div>
 		)
 	}
 
 	return (
 		<div class="flex flex-col gap-4 h-50vh">
-			{/* TODO: 2-cols style */}
-			{/* <Show when={isSmall() || stack().length < 2} fallback={<DoubleCol />}>
-				<SingleCol />
-			</Show> */}
 			<SingleCol />
 			<div class="flex gap-2 text-xs">
 				<span>{t('currentPath')}:</span>
