542b22e3730560f63f51a490d67b574c71c28408
miaobuao <miaobuao@outlook.com>
2025-05-14 14:29:08 +0800

    feat: enhance intelligent merge strategy for conflict resolution

--- src/sync/tasks/conflict-resolve.task.ts
+++ src/sync/tasks/conflict-resolve.task.ts
@@ -1,6 +1,5 @@
-import { diff_match_patch } from 'diff-match-patch'
-import { isEqual } from 'lodash-es'
-import { moment } from 'obsidian'
+import { isEqual, noop } from 'lodash-es'
+import { isNotNil } from 'ramda'
 import { BufferLike } from 'webdav'
 import i18n from '~/i18n'
 import { StatModel } from '~/model/stat.model'
@@ -11,10 +10,15 @@ import logger from '~/utils/logger'
 import { mergeDigIn } from '~/utils/merge-dig-in'
 import { statVaultItem } from '~/utils/stat-vault-item'
 import { statWebDAVItem } from '~/utils/stat-webdav-item'
+import {
+	LatestTimestampResolution,
+	resolveByIntelligentMerge,
+	resolveByLatestTimestamp,
+} from '../core/merge-utils'
 import { BaseTask, BaseTaskOptions, toTaskError } from './task.interface'
 
 export enum ConflictStrategy {
-	DiffMatchPatch,
+	IntelligentMerge,
 	LatestTimeStamp,
 }
 
@@ -36,21 +40,26 @@ export default class ConflictResolveTask extends BaseTask {
 			const local =
 				this.options.localStat ??
 				(await statVaultItem(this.vault, this.localPath))
+
 			if (!local) {
 				throw new Error('Local file not found: ' + this.localPath)
 			}
+
 			const remote =
 				this.options.remoteStat ??
 				(await statWebDAVItem(this.webdav, this.remotePath))
+
 			if (remote.isDir) {
 				throw new Error('Remote path is a directory: ' + this.remotePath)
 			}
+
 			if (local.size === 0 && remote.size === 0) {
 				return { success: true }
 			}
+
 			switch (this.options.strategy) {
-				case ConflictStrategy.DiffMatchPatch:
-					return await this.execDiffMatchPatch()
+				case ConflictStrategy.IntelligentMerge:
+					return await this.execIntelligentMerge()
 				case ConflictStrategy.LatestTimeStamp:
 					return await this.execLatestTimeStamp(local, remote)
 			}
@@ -65,94 +74,147 @@ export default class ConflictResolveTask extends BaseTask {
 
 	async execLatestTimeStamp(local: StatModel, remote: StatModel) {
 		try {
-			const localMtime = moment(local.mtime)
-			const remoteMtime = moment(remote.mtime)
-			if (remoteMtime.isSame(localMtime)) {
-				// If local and remote timestamps are identical, no conflict resolution is needed.
-				return {
-					success: true,
-				}
+			const localMtime = local.mtime
+			const remoteMtime = remote.mtime
+
+			if (remoteMtime === localMtime) {
+				return { success: true }
 			}
+
 			const localContent = await this.vault.adapter.readBinary(this.localPath)
-			const useRemote = remoteMtime.isAfter(localMtime)
-			if (useRemote) {
-				const remoteContent = (await this.webdav.getFileContents(
-					this.remotePath,
-					{
-						details: false,
-						format: 'binary',
-					},
-				)) as BufferLike
-				if (isEqual(localContent, remoteContent)) {
-					await this.vault.adapter.writeBinary(this.localPath, remoteContent)
-				}
-			} else {
-				await this.webdav.putFileContents(this.remotePath, localContent, {
-					overwrite: true,
-				})
+			const remoteContent = (await this.webdav.getFileContents(
+				this.remotePath,
+				{
+					details: false,
+					format: 'binary',
+				},
+			)) as BufferLike
+
+			const result = resolveByLatestTimestamp({
+				localMtime,
+				remoteMtime,
+				localContent,
+				remoteContent,
+			})
+
+			switch (result.status) {
+				case LatestTimestampResolution.UseRemote:
+					await this.vault.adapter.writeBinary(this.localPath, result.content)
+					break
+				case LatestTimestampResolution.UseLocal:
+					await this.webdav.putFileContents(this.remotePath, result.content, {
+						overwrite: true,
+					})
+					break
+				case LatestTimestampResolution.NoChange:
+					noop()
+					break
 			}
+
 			return { success: true }
 		} catch (e) {
-			logger.error(e)
+			logger.error(this, e)
 			return { success: false, error: toTaskError(e, this) }
 		}
 	}
 
-	async execDiffMatchPatch() {
+	async execIntelligentMerge() {
 		try {
 			const localBuffer = await this.vault.adapter.readBinary(this.localPath)
 			const remoteBuffer = (await this.webdav.getFileContents(this.remotePath, {
 				format: 'binary',
 				details: false,
 			})) as BufferLike
-			if (await isEqual(localBuffer, remoteBuffer)) {
+
+			if (isEqual(localBuffer, remoteBuffer)) {
 				return { success: true }
 			}
-			if (await isBinaryFile(localBuffer)) {
-				throw new Error(i18n.t('sync.error.cannotMergeBinary'))
-			}
-			const localText = await new Blob([localBuffer]).text()
-			const remoteText = await new Blob([remoteBuffer]).text()
+
 			const { record } = this.options
 			let baseBlob: Blob | null = null
 			const baseKey = record?.base?.key
 			if (baseKey) {
 				baseBlob = await blobStore.get(baseKey)
 			}
+
+			const hasBinaryFile = await Promise.all(
+				[localBuffer, remoteBuffer, await baseBlob?.arrayBuffer()]
+					.filter(isNotNil)
+					.map((buf) => isBinaryFile(buf)),
+			).then((res) => res.includes(true))
+
+			if (hasBinaryFile) {
+				throw new Error(i18n.t('sync.error.cannotMergeBinary'))
+			}
+
+			const localText = await new Blob([localBuffer]).text()
+			const remoteText = await new Blob([remoteBuffer]).text()
 			const baseText = (await baseBlob?.text()) ?? localText
-			const dmp = new diff_match_patch()
-			dmp.Match_Threshold = 0.2
-			dmp.Patch_Margin = 4
-			const diffs = dmp.diff_main(baseText, remoteText)
-			const patches = dmp.patch_make(baseText, diffs)
-			let [mergedText, solveResult] = dmp.patch_apply(patches, localText)
-			if (solveResult.includes(false)) {
-				const diff3MergedResult = mergeDigIn(localText, baseText, remoteText, {
+
+			const mergeResult = await resolveByIntelligentMerge({
+				localContentText: localText,
+				remoteContentText: remoteText,
+				baseContentText: baseText,
+			})
+
+			if (!mergeResult.success) {
+				// If patch_apply fails to resolve all, use mergeDigIn as a further fallback
+				const mergeDigInResult = mergeDigIn(localText, baseText, remoteText, {
 					stringSeparator: '\n',
 					useGitStyle: this.options.useGitStyle,
 				})
-				mergedText = diff3MergedResult.result.join('\n')
+				// mergeDigIn itself might produce conflict markers if it can't fully resolve.
+				// The task should handle this merged text (which might contain markers).
+				const mergedDmpText = mergeDigInResult.result.join('\n')
+
+				const putResult = await this.webdav.putFileContents(
+					this.remotePath,
+					mergedDmpText,
+					{ overwrite: true },
+				)
+
+				if (putResult) {
+					await this.vault.adapter.write(this.localPath, mergedDmpText)
+					return { success: true }
+				} else {
+					throw new Error(i18n.t('sync.error.failedToUploadMerged'))
+				}
 			}
+
+			if (mergeResult.isIdentical) {
+				// This case should be caught by the isEqual(localBuffer, remoteBuffer) check earlier,
+				// but resolveByIntelligentMerge also returns it.
+				return { success: true }
+			}
+
+			const mergedText = mergeResult.mergedText!
+
+			// If mergedText is the same as remoteText, we only need to update localText if it's different.
 			if (mergedText === remoteText) {
 				if (mergedText !== localText) {
 					await this.vault.adapter.write(this.localPath, mergedText)
 				}
-				return {
-					success: true,
-				}
+				return { success: true }
 			}
+
+			// If mergedText is different from remoteText, then both remote and local need to be updated.
 			const putResult = await this.webdav.putFileContents(
 				this.remotePath,
 				mergedText,
 				{ overwrite: true },
 			)
+
 			if (!putResult) {
 				throw new Error(i18n.t('sync.error.failedToUploadMerged'))
 			}
-			await this.vault.adapter.write(this.localPath, mergedText)
+
+			if (localText !== mergedText) {
+				await this.vault.adapter.write(this.localPath, mergedText)
+			}
+
 			return { success: true }
 		} catch (e) {
-			logger.error(e)
+			logger.error(this, e)
 			return { success: false, error: toTaskError(e, this) }
 		}
 	}
