b9dcc6aad8f8e0f449e24cd538ffe32a32e8648d
miaobuao <miaobuao@outlook.com>
2025-06-26 15:10:02 +0800

    fix: improve file handling in PullTask exec method

--- src/sync/tasks/pull.task.ts
+++ src/sync/tasks/pull.task.ts
@@ -6,17 +6,40 @@ import { BaseTask, toTaskError } from './task.interface'
 
 export default class PullTask extends BaseTask {
 	async exec() {
+		const fileExists = await this.vault.getFileByPath(this.localPath)
 		try {
-			await mkdirsVault(this.vault, dirname(this.localPath))
 			const file = (await this.webdav.getFileContents(this.remotePath, {
 				format: 'binary',
 				details: false,
 			})) as BufferLike
-			await this.vault.createBinary(this.localPath, file)
+			const arrayBuffer = bufferLikeToArrayBuffer(file)
+			if (fileExists) {
+				await this.vault.modifyBinary(fileExists, arrayBuffer)
+			} else {
+				await mkdirsVault(this.vault, dirname(this.localPath))
+				await this.vault.createBinary(this.localPath, arrayBuffer)
+			}
 			return { success: true }
 		} catch (e) {
 			logger.error(this, e)
 			return { success: false, error: toTaskError(e, this) }
 		}
 	}
+}
+
+function bufferLikeToArrayBuffer(buffer: BufferLike) {
+	if (buffer instanceof ArrayBuffer) {
+		return buffer
+	} else {
+		return toArrayBuffer(buffer)
+	}
+}
+
+function toArrayBuffer(buffer: Buffer) {
+	const arrayBuffer = new ArrayBuffer(buffer.length)
+	const view = new Uint8Array(arrayBuffer)
+	for (let i = 0; i < buffer.length; ++i) {
+		view[i] = buffer[i]
+	}
+	return arrayBuffer
 }
