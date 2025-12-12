bf330197726532cb55eaa8bb1724682626fd4be5
miaobuao <miaobuao@outlook.com>
2025-12-12 11:13:27 +0800

    feat: auto exclude configDir

--- src/services/sync-executor.service.ts
+++ src/services/sync-executor.service.ts
@@ -23,6 +23,19 @@ export default class SyncExecutorService {
 
 		await waitUntil(() => this.plugin.isSyncing === false, 500)
 
+		// 确保 configDir 始终在排除列表中，因为这个目录里的文件不支持同步
+		const configDir = this.plugin.app.vault.configDir
+		const hasConfigDirRule = this.plugin.settings.filterRules.exclusionRules.some(
+			(rule) => rule.expr === configDir
+		)
+		if (!hasConfigDirRule) {
+			this.plugin.settings.filterRules.exclusionRules.push({
+				expr: configDir,
+				options: { caseSensitive: false },
+			})
+			await this.plugin.saveSettings()
+		}
+
 		const sync = new NutstoreSync(this.plugin, {
 			vault: this.plugin.app.vault,
 			token: await this.plugin.getToken(),
