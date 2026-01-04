cd5de566eddcc325384f9f2f938dc0fd01873c7b
miaobuao <miaobuao@outlook.com>
2026-01-04 18:27:43 +0800

    feat: user-agent

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,7 @@ import { Platform, requireApiVersion } from 'obsidian'
 
 export const NS_NSDAV_ENDPOINT = process.env.NS_NSDAV_ENDPOINT!
 export const NS_DAV_ENDPOINT = process.env.NS_DAV_ENDPOINT!
+export const PLUGIN_VERSION = process.env.PLUGIN_VERSION!
 
 export const API_VER_STAT_FOLDER = '0.13.27'
 export const API_VER_REQURL = '0.13.26' // desktop ver 0.13.26, iOS ver 1.1.1
--- src/utils/request-url.ts
+++ src/utils/request-url.ts
@@ -1,9 +1,30 @@
 import {
+	Platform,
 	requestUrl as req,
 	RequestUrlParam,
 	RequestUrlResponse,
 } from 'obsidian'
+import { PLUGIN_VERSION } from '~/consts'
 import logger from './logger'
+
+const getOS = () => {
+	if (Platform.isWin) return 'Windows'
+	if (Platform.isMacOS) return 'macOS'
+	if (Platform.isLinux) return 'Linux'
+	if (Platform.isAndroidApp) return 'Android'
+	if (Platform.isIosApp) return 'iOS'
+	return 'Unknown'
+}
+
+const getDevice = () => {
+	if (Platform.isTablet) return 'Tablet'
+	if (Platform.isPhone) return 'Phone'
+	if (Platform.isDesktopApp) return 'Desktop'
+	if (Platform.isMobileApp) return 'Mobile'
+	return 'Unknown'
+}
+
+const USER_AGENT = `Obsidian (${getOS()}; ${getDevice()}; ObsidianNutstoreSync/${PLUGIN_VERSION})`
 
 class RequestUrlError extends Error {
 	constructor(public res: RequestUrlResponse) {
@@ -12,27 +33,28 @@ class RequestUrlError extends Error {
 }
 
 export default async function requestUrl(p: RequestUrlParam | string) {
-	let res: RequestUrlResponse
-	let throwError = true
-	if (typeof p === 'string') {
-		res = await req({
-			url: p,
-			throw: false,
-		})
-	} else if (p.throw !== false) {
-		res = await req({
-			...p,
-			throw: false,
-		})
-	} else {
-		res = await req(p)
-		throwError = false
-	}
+	const params: RequestUrlParam =
+		typeof p === 'string'
+			? {
+					url: p,
+					throw: false,
+				}
+			: {
+					...p,
+					throw: false,
+					headers: {
+						...(p.headers || {}),
+					},
+				}
+
+	const res = await req(params)
+
 	if (res.status >= 400) {
 		logger.error(res)
-		if (throwError) {
+		if (typeof p === 'string' || p.throw !== false) {
 			throw new RequestUrlError(res)
 		}
 	}
+
 	return res
 }
